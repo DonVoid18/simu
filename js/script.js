964245e847ef606447a394a217bb00bb4bfccc32
Patrick <donvoid18@gmail.com>
2023-12-18 03:03:43 -0500

    simutation image

--- js/script.js
+++ js/script.js
@@ -1,64 +1,52 @@
 const inter = 1
+const clients = document.querySelectorAll(".client")
 
-const client1 = document.getElementById("client1")
-const client2 = document.getElementById("client2")
-const client3 = document.getElementById("client3")
-const client4 = document.getElementById("client4")
+// Establecer posición inicial de los clientes
+clients.forEach((client, index) => {
+  client.style.position = "absolute"
+  client.style.top = `${index * 110}px`
+  client.style.left = "0px"
+})
 
-client1.style.position = "absolute"
-client1.style.top = "0px"
-client1.style.left = "0px"
+// Función para mover un cliente
+function moveClient(client, speed) {
+  let nuevaPosicionLeft = 0
+  const intervalId = setInterval(() => {
+    nuevaPosicionLeft = parseFloat(client.style.left) + speed
+    client.style.left = nuevaPosicionLeft + "px"
 
-client2.style.position = "absolute"
-client2.style.top = "200px"
-client2.style.left = "0px"
+    if (nuevaPosicionLeft >= 1400) {
+      clearInterval(intervalId)
+      client.style.display = "none" // Oculta la imagen
+    }
+  }, inter)
+}
 
-client3.style.position = "absolute"
-client3.style.top = "400px"
-client3.style.left = "0px"
-
-client4.style.position = "absolute"
-client4.style.top = "600px"
-client4.style.left = "0px"
-
-let nuevaPosicionLeft = 0  // Declarar nuevaPosicionLeft fuera del setInterval
-
-const intervalId = setInterval(() => {
-  nuevaPosicionLeft = parseFloat(client1.style.left) + 0.5
-  client1.style.left = nuevaPosicionLeft + "px"
-
-  if (nuevaPosicionLeft >= 1200) {
-    clearInterval(intervalId)
-    client1.style.display = "none" // Oculta la imagen
+// Mover cada cliente con una velocidad diferente
+clients.forEach((client, index) => {
+  console.log(client, index)
+  switch (index) {
+    case 0:
+      moveClient(client, 0.5)
+      break
+    case 1:
+      moveClient(client, 0.8)
+      break
+    case 2:
+      moveClient(client, 0.3)
+      break
+    case 3:
+      moveClient(client, 0.5)
+      break
+    case 4:
+      moveClient(client, 0.7)
+      break
+    case 5:
+      moveClient(client, 0.9)
+      break
+    case 6:
+      moveClient(client, 0.4)
+      break
+    // Puedes añadir más casos si hay más clientes
   }
-}, inter)
-
-const intervalId2 = setInterval(() => {
-  nuevaPosicionLeft = parseFloat(client2.style.left) + 0.8
-  client2.style.left = nuevaPosicionLeft + "px"
-
-  if (nuevaPosicionLeft >= 1200) {
-    clearInterval(intervalId2)
-    client2.style.display = "none" // Oculta la imagen
-  }
-}, inter)
-
-const intervalId3 = setInterval(() => {
-  nuevaPosicionLeft = parseFloat(client3.style.left) + 0.3
-  client3.style.left = nuevaPosicionLeft + "px"
-
-  if (nuevaPosicionLeft >= 1200) {
-    clearInterval(intervalId3)
-    client3.style.display = "none" // Oculta la imagen
-  }
-}, inter)
-
-const intervalId4 = setInterval(() => {
-  nuevaPosicionLeft = parseFloat(client4.style.left) + 0.5
-  client4.style.left = nuevaPosicionLeft + "px"
-
-  if (nuevaPosicionLeft >= 1200) {
-    clearInterval(intervalId4)
-    client4.style.display = "none" // Oculta la imagen
-  }
-}, inter)
+})
